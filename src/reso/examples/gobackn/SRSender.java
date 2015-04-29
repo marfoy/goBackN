f38e2eeb23fd7c355f7d165975505553763b396e
florentdelgrange <florent.delgrange@gmail.com>
2015-04-29 12:08:26 +0200

    Ajout du SRSender

---
+++ src/reso/examples/gobackn/SRSender.java
@@ -0,0 +1,14 @@
+package reso.examples.gobackn;
+
+/**
+ * Created by florentdelgrange on 29/04/15.
+ */
+public class SRSender {
+
+    int sendBase;
+    int nextSeqNum;
+
+    public void rdtSend(int data){
+
+    }
+}
