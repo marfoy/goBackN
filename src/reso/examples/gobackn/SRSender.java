6bd190cdd40f75d0d56d3115df69f1b2b88ca45c
florentdelgrange <florent.delgrange@gmail.com>
2015-05-06 12:05:48 +0200

    Ajout du Sender : tres evolue en plus le code est indente, parfait. PS : certaines fonctionnalites ne sont pas omplementees, veuillez vous referer au RFC

--- src/reso/examples/gobackn/SRSender.java
+++ src/reso/examples/gobackn/SRSender.java
@@ -9,6 +9,17 @@ public class SRSender {
     int nextSeqNum;
 
     public void rdtSend(int data){
+        if (nextSeqNum < sendBase + N) {
+            pkt = make_pkt(next_seq_num, data, checksum);
+            window[nextSeqNum] = pkt;
+            udt_send(pkt);
+            if (sendBase == nextSeqNum)
+                start_timer();
+            nextSeqNum++;
 
+        }
+        else
+            refuse data;
     }
+
 }
